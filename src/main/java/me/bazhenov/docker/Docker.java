76b31e8e01482d0064886a5a2dd351cb2e60379c
Pierre Garrabos <pierre.garrabos@total.com>
2020-02-21 11:31:06 +0100

    Be compatible with IPv6 system

--- src/main/java/me/bazhenov/docker/Docker.java
+++ src/main/java/me/bazhenov/docker/Docker.java
@@ -33,6 +33,8 @@ import static org.slf4j.LoggerFactory.getLogger;
 @SuppressWarnings("WeakerAccess")
 public final class Docker implements Closeable {
 
+	private static final String IPV6_FILE_PATH = "/proc/self/net/tcp6";
+    
 	private static final Logger log = getLogger(Docker.class);
 	private static final ObjectMapper jsonReader = new ObjectMapper();
 
@@ -259,7 +261,8 @@ public final class Docker implements Closeable {
 		while (!self.isInterrupted()) {
 			Set<Integer> openPorts = new HashSet<>();
 			openPorts.addAll(readListenPorts(docker("exec", cid, "cat", "/proc/self/net/tcp")));
-			openPorts.addAll(readListenPorts(docker("exec", cid, "cat", "/proc/self/net/tcp6")));
+		    openPorts.addAll(readListenPorts(docker("exec", cid, "sh", "-c", "if [ -f " + IPV6_FILE_PATH + " ]; then cat " + IPV6_FILE_PATH + "; fi")));
+			
 			if (openPorts.containsAll(ports))
 				return;
 
@@ -274,7 +277,7 @@ public final class Docker implements Closeable {
 		}
 	}
 
-	private String docker(String command, String... args) throws IOException, InterruptedException {
+  private String docker(String command, String... args) throws IOException, InterruptedException {
 		List<String> cmd = new ArrayList<>(args.length + 2);
 		cmd.add(pathToDocker);
 		cmd.add(command);
